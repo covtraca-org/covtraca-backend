821f579a4832c0d3997330460ac958e0b6958d87
Craig <m@crdev.de>
2020-04-19 16:03:30 +0100

    Initial

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use \App\Record;
+
+class ApiController extends Controller
+{
+    /**
+     * We dont need api authentication at this point
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return void
+     * @version   GIT: $Id; Crdev ltd. 2019 $
+     */
+    public function put(Request $request)
+    {
+        $data = [
+            'uid' => $request->get('uid'),
+            'long' => $request->get('long'),
+            'lat' => $request->get('lat'),
+        ];
+
+        $model = new Records($data);
+        if ($model->save()) {
+            return $model;
+        }
+
+        header("", true, 400);
+    }
+}
---
+++ database/migrations/2020_04_19_143034_create_records_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('records', function (Blueprint $table) {
+            $table->id();
+            $table->varchar('uid', 32);
+            $table->float('lat');
+            $table->float('long');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('records');
+    }
+}
